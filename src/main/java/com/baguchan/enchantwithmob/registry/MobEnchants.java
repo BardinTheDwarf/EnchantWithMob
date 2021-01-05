f86fd9e7741a576aa242f9f90a44e7bf24c89744
shutux <shutux500@gmail.com>
2021-01-05 12:25:54 +0900

    fix uuid dupe and add tough enchant

--- src/main/java/com/baguchan/enchantwithmob/registry/MobEnchants.java
+++ src/main/java/com/baguchan/enchantwithmob/registry/MobEnchants.java
@@ -14,10 +14,11 @@ import net.minecraftforge.registries.RegistryBuilder;
 @Mod.EventBusSubscriber(modid = EnchantWithMob.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class MobEnchants {
     public static final MobEnchant PROTECTION = new ProtectionMobEnchant(new MobEnchant.Properties(MobEnchant.Rarity.COMMON, 4));
-    public static final MobEnchant SPEEDY = new SpeedyMobEnchant(new MobEnchant.Properties(MobEnchant.Rarity.UNCOMMON, 2)).addAttributesModifier(Attributes.MOVEMENT_SPEED, "91AEAA56-376B-4498-935B-2F7F68070635", (double) 0.05F, AttributeModifier.Operation.ADDITION);
+    public static final MobEnchant TOUGH = new ProtectionMobEnchant(new MobEnchant.Properties(MobEnchant.Rarity.VERY_RARE, 2)).addAttributesModifier(Attributes.ARMOR, "313644c5-ead2-4670-b9eb-0b41d59ce5a2", (double) 2.0F, AttributeModifier.Operation.ADDITION).addAttributesModifier(Attributes.ARMOR_TOUGHNESS, "8135df8f-38d9-490a-8d6f-c908fa973b34", (double) 0.5F, AttributeModifier.Operation.ADDITION);
+    public static final MobEnchant SPEEDY = new SpeedyMobEnchant(new MobEnchant.Properties(MobEnchant.Rarity.UNCOMMON, 2)).addAttributesModifier(Attributes.MOVEMENT_SPEED, "501f27a9-4a75-4c2e-a2ab-91eeed71d748", (double) 0.05F, AttributeModifier.Operation.ADDITION);
     public static final MobEnchant STRONG = new StrongMobEnchant(new MobEnchant.Properties(MobEnchant.Rarity.COMMON, 3));
     public static final MobEnchant THORN = new ThronEnchant(new MobEnchant.Properties(MobEnchant.Rarity.VERY_RARE, 3));
-    public static final MobEnchant HEALTH_BOOST = new HealthBoostMobEnchant(new MobEnchant.Properties(MobEnchant.Rarity.VERY_RARE, 5)).addAttributesModifier(Attributes.MAX_HEALTH, "5D6F0BA2-1186-46AC-B896-C61C5CEE99CC", 2.0D, AttributeModifier.Operation.ADDITION);
+    public static final MobEnchant HEALTH_BOOST = new HealthBoostMobEnchant(new MobEnchant.Properties(MobEnchant.Rarity.VERY_RARE, 5)).addAttributesModifier(Attributes.MAX_HEALTH, "f5d32c9f-2a3d-4157-bbf7-469d348ce097", 2.0D, AttributeModifier.Operation.ADDITION);
 
 
     private static ForgeRegistry<MobEnchant> registry;
@@ -35,6 +36,7 @@ public class MobEnchants {
     @SubscribeEvent
     public static void onRegisterEnchant(RegistryEvent.Register<MobEnchant> event) {
         event.getRegistry().registerAll(PROTECTION.setRegistryName("protection"),
+                TOUGH.setRegistryName("tough"),
                 SPEEDY.setRegistryName("speedy"),
                 STRONG.setRegistryName("strong"),
                 THORN.setRegistryName("thorn"),
